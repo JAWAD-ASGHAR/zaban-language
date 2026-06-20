ed49ec1aa103eb884c5e848f4adb3fc72db5d614
Ronin <abdullahrazi60@gmail.com>
2026-06-21 00:27:18 +0500

    working lexer-parser-interpreter V1

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,8 @@ const source = fs.readFileSync(filePath, "utf8");
 
 const tokens = tokenize(source);
 
-for (const token of tokens) {
-  if (token.type === "EOF") break;
-  console.log(`${token.type} : ${token.value}`);
-}
-
 const parser = new Parser(tokens);
 const program = parser.parse();
-
-console.log(JSON.stringify(program, null, 2));
 
 const interpreter = new Interpreter();
 interpreter.interpret(program);
